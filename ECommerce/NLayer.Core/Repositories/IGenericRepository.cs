00f7b99509f9fca14cc06b231c11102f18234e58
Furkan Fatih ŞAFAK <fatihsafakutarit@gmail.com>
2023-04-26 12:01:56 +0300

    E Coommerce
    Asp .Net Core 6 N Layer E Coommerce

---
+++ ECommerce/NLayer.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.Repositories
+{
+    public class IGenericRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+    }
+}
